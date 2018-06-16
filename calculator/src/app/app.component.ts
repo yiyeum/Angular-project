b57536e972c9237d107f8fab91bc41bba80e3029
Kate Yeeum <yeeumm@gmail.com>
2018-06-16 15:51:25 -0700

    Operation function created / display the result in the result div

--- calculator/src/app/app.component.ts
+++ calculator/src/app/app.component.ts
@@ -5,6 +5,41 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  public sumResult : number;
+  public diffResult : number;
+  public prodResult : number;
+  public quoResult : number;
+  
+  // function - sum
+  sum(value1: string, value2: string){
+    this.sumResult = parseInt(value1) + parseInt(value2);
+  }
+
+  // function - difference
+  diff(value1: string, value2: string){
+    this.diffResult = parseInt(value1) - parseInt(value2);
+  }
+
+  // function - product
+  prod(value1: string, value2: string){
+    this.prodResult = parseInt(value1) * parseInt(value2);
+  }
+
+  // function - quo
+  quo(value1: string, value2: string){
+    this.quoResult = parseInt(value1) / parseInt(value2);
+  }
+
+  public sumValue1 : string;
+  public sumValue2 : string;
+
+  public diffValue1 : string;
+  public diffValue2 : string;
+  public prodValue1 : string;
+  public prodValue2 : string;
+  public quoValue1 : string;
+  public quoValue2 : string;
+
 }
