1a7b65a21aebce403027608042e62ca8af750cab
Kate Yeeum <yeeumm@gmail.com>
2018-06-16 18:14:07 -0700

    Clean all the inputs (repeated code is allowed for now) when clicking the = button

--- calculator/src/app/app.component.ts
+++ calculator/src/app/app.component.ts
@@ -7,39 +7,78 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  public sumResult : number;
-  public diffResult : number;
-  public prodResult : number;
-  public quoResult : number;
-  
-  // function - sum
-  sum(value1: string, value2: string){
-    this.sumResult = parseInt(value1) + parseInt(value2);
-  }
 
-  // function - difference
-  diff(value1: string, value2: string){
-    this.diffResult = parseInt(value1) - parseInt(value2);
-  }
-
-  // function - product
-  prod(value1: string, value2: string){
-    this.prodResult = parseInt(value1) * parseInt(value2);
-  }
-
-  // function - quo
-  quo(value1: string, value2: string){
-    this.quoResult = parseInt(value1) / parseInt(value2);
-  }
-
+  // Value vars
   public sumValue1 : string;
   public sumValue2 : string;
-
   public diffValue1 : string;
   public diffValue2 : string;
   public prodValue1 : string;
   public prodValue2 : string;
   public quoValue1 : string;
   public quoValue2 : string;
+
+  // Result vars
+  public sumResult : number;
+  public diffResult : number;
+  public prodResult : number;
+  public quoResult : any;
+
+  // function - sum
+  sum(value1: string, value2: string){
+    this.sumResult = parseFloat(value1) + parseFloat(value2);
+      this.diffValue1 = null;
+      this.diffValue2 = null;
+      this.prodValue1 = null;
+      this.prodValue2 = null;
+      this.quoValue1 = null;
+      this.quoValue2 = null;
+      this.diffResult = null;
+      this.prodResult = null;
+      this.quoResult = null;
+  }
+
+  // function - difference
+  diff(value1: string, value2: string){
+    this.diffResult = parseFloat(value1) - parseFloat(value2);
+    this.sumValue1 = null;
+    this.sumValue2 = null;
+    this.prodValue1 = null;
+    this.prodValue2 = null;
+    this.quoValue1 = null;
+    this.quoValue2 = null;
+    this.sumResult = null;
+    this.prodResult = null;
+    this.quoResult = null;
+  }
+
+  // function - product
+  prod(value1: string, value2: string){
+    this.prodResult = parseFloat(value1) * parseFloat(value2);
+    this.sumValue1 = null;
+    this.sumValue2 = null;
+    this.diffValue1 = null;
+    this.diffValue2 = null;
+    this.quoValue1 = null;
+    this.quoValue2 = null;
+    this.sumResult = null;
+    this.diffResult = null;
+    this.quoResult = null;
+  }
+
+  // function - quo
+  quo(value1: string, value2: string){
+    this.quoResult = ((parseFloat(value1) / parseFloat(value2))).toFixed(2);
+    this.sumValue1 = null;
+    this.sumValue2 = null;
+    this.diffValue1 = null;
+    this.diffValue2 = null;
+    this.prodValue1 = null;
+    this.prodValue2 = null;
+    this.sumResult = null;
+    this.diffResult = null;
+    this.prodResult = null;
+  }
+
 
 }
